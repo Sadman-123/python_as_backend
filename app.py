cdf216479ef1516a6692138ad4dc2467fb1fddfe
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-06-02 23:31:59 +0600

    all done

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify,json
+from flask_cors import CORS
+app=Flask(__name__)
+CORS(app)
+@app.route('/')
+def inital():
+    return 'Hello World'
+@app.route('/data')
+def first():
+    dat={
+        "name":"Shadman Sakib Mahee",
+        "ID":19201103123,
+        "Dept":"CSE",
+    }
+    return jsonify(dat)
+app.run()
